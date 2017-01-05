d3330c962f3c4996faa149fa7d2ab6476b2daa60
Ronnie <ronnietran1@hotmail.com>
2017-01-04 20:01:44 -0500

    Remove unused code

--- src/main/java/com/github/shaigem/linkgem/util/LocationUtil.java
+++ src/main/java/com/github/shaigem/linkgem/util/LocationUtil.java
@@ -4,41 +4,9 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 
 /**
- * Created on 2016-12-30.
+ * Contains utilities for locations.
  */
 public class LocationUtil {
-
-
-    /**
-     * Gets the extension of a string. This is used for fetching favicon extensions.
-     *
-     * @param string the string to get the extension from
-     * @return the given string's extension
-     */
-    public static String getExtension(String string) {
-        String extension = "";
-
-        int lastIndex = string.lastIndexOf('.');
-        int p = Math.max(string.lastIndexOf('/'), string.lastIndexOf('\\'));
-
-        if (lastIndex > p) {
-            extension = string.substring(lastIndex + 1);
-        }
-        return extension;
-    }
-
-
-    public static String addHostAndProtocol(String baseUrl, String url) {
-        if (!url.equals("") && !url.startsWith("http") && !url.startsWith("https") && !url.startsWith("wwww")) {
-            if (url.startsWith("//")) {
-                url = url.substring(2);
-            } else if (url.startsWith("/"))
-                url = baseUrl + url;
-            else url = baseUrl + "/" + url;
-        }
-        return addHttp(url);
-    }
-
 
     public static boolean validLocation(String string) {
         return string.matches("^(https?|ftp)://.*$");
