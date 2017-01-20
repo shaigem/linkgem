74d35e62f5525cfe31357fcb344e14cd3aef5e54
Ronnie <ronnietran1@hotmail.com>
2017-01-20 15:13:43 -0500

    Add save button to the main toolbar

--- src/main/java/com/github/shaigem/linkgem/fx/MainToolbar.java
+++ src/main/java/com/github/shaigem/linkgem/fx/MainToolbar.java
@@ -4,6 +4,7 @@ import com.github.shaigem.linkgem.ui.search.SearchPresenter;
 import com.github.shaigem.linkgem.ui.search.SearchView;
 import de.jensd.fx.glyphs.GlyphsDude;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import de.jensd.fx.glyphs.materialdesignicons.MaterialDesignIcon;
 import javafx.scene.control.Button;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
@@ -28,14 +29,14 @@ public class MainToolbar extends CustomToolbar {
         getLeftSection().getChildren().addAll(logoView);
 
 
-        //   final Button saveAllButton = button(GlyphsDude.createIcon(MaterialDesignIcon.CONTENT_SAVE_ALL, "1.8em")
-        //         , "Save all changes");
+           final Button saveAllButton = button(GlyphsDude.createIcon(MaterialDesignIcon.CONTENT_SAVE_ALL, "1.8em")
+                 , "Save All");
 
         searchView = new SearchView();
         final Button aboutButton =
                 button(GlyphsDude.createIcon(FontAwesomeIcon.COG, "1.8em"), "");
 
-        getRightSection().getChildren().addAll(searchView.getViewWithoutRootContainer(), /*saveAllButton ,*/ aboutButton);
+        getRightSection().getChildren().addAll(searchView.getViewWithoutRootContainer(), saveAllButton , aboutButton);
         eventStudio().addAnnotatedListeners(this);
     }
 
@@ -48,8 +49,10 @@ public class MainToolbar extends CustomToolbar {
     }
 
     private Button button(Text icon, String tooltipText) {
-        Button button = button(tooltipText);
-        button.setTooltip(new Tooltip(tooltipText));
+        Button button = button("");
+        if(!tooltipText.isEmpty()) {
+            button.setTooltip(new Tooltip(tooltipText));
+        }
         button.setGraphic(icon);
         return button;
     }
